935f3e708118bfacfdd93af4878392aa480eb23c
Eduardo Segura <eduardo@zxxxro.com>
2025-03-11 20:51:15 -0300

    feat: added array sortedIndex helper

---
+++ src/common/services/List.ts
@@ -0,0 +1,18 @@
+
+export class List {
+
+  public static getSortedIndex<T>(array: Array<T>, isLessThan: (current: T) => boolean) {
+    let low = 0
+    let high = array.length;
+  
+    while (low < high) {
+      const mid = low + high >>> 1;
+      if (isLessThan(array[mid])) low = mid + 1;
+      else high = mid;
+    }
+    return low;
+  }
+  
+}
+
+export default List
