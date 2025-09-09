27babc5be0530490e39f7f3ab185723b57484382
Eduardo Segura <eduardo@zxxxro.com>
2025-09-08 23:46:08 -0300

    chore: add exceptions publish files

---
+++ exceptions.ts
@@ -0,0 +1,7 @@
+import AnnotationException from '~/decorator/exceptions/annotation.exception.ts';
+
+export { AnnotationException } from '~/decorator/exceptions/annotation.exception.ts';
+
+export default {
+  AnnotationException,
+}
