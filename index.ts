9b5918e7848e2de2508287ec4dabe6f3b1772745
Eduardo Segura <eduardo@zxxxro.com>
2025-09-08 23:50:14 -0300

    chore: add exceptions publish files

--- index.ts
+++ index.ts
@@ -1,19 +1,23 @@
 import Annotations from './annotations.ts'
 import Enums from './enums.ts'
+import Exceptions from './exceptions.ts'
 import Guards from './guards.ts'
 import Services from './services.ts'
 import Validations from './validations.ts';
 
 export * from './annotations.ts'
 export * from './enums.ts'
+export * from './exceptions.ts'
 export * from './guards.ts'
 export * from './services.ts'
 export * from './validations.ts'
 
 export { default as Annotations } from './annotations.ts'
 export { default as Enums } from './enums.ts'
+export { default as Exceptions } from './exceptions.ts'
 export { default as Guards } from './guards.ts'
 export { default as Services } from './services.ts'
+export { default as Validations } from './services.ts'
 
 export * from '~/common/interfaces.ts';
 export * from '~/common/types.ts';
@@ -27,6 +31,7 @@ export * from '~/validator/types.ts';
 export default {
   ...Annotations,
   ...Enums,
+  ...Exceptions,
   ...Guards,
   ...Services,
   ...Validations
