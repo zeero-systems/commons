868383c46d244a05d355304c21d4fb915f82716f
Eduardo Segura <eduardo@zxxxro.com>
2025-10-02 11:21:44 -0300

    fix: on argument types in text methods

--- src/common/services/text.service.ts
+++ src/common/services/text.service.ts
@@ -5,13 +5,13 @@
  * @param {string} toFirstLetterUppercase - Return the string with his first letter upcased
  */ 
 export class Text {
-  public static toFirstLetterUppercase(text?: string | symbol | string): string {
+  public static toFirstLetterUppercase(text?: string | number | symbol): string {
     return `${String(text).charAt(0).toUpperCase()}${String(text).slice(1)}`;
   }
-  public static toSnakecase(text?: string | symbol | string): string {
+  public static toSnakecase(text?: string | number | symbol): string {
     return String(text).replace(/([A-Z])/g, "_$1").toLowerCase();
   }
-  public static toCamelcase(text?: string | symbol | string): string {
+  public static toCamelcase(text?: string | number | symbol): string {
     return String(text).replace(/[^a-zA-Z0-9]+(.)?/g, (_match, chr) => chr ? chr.toUpperCase() : '').replace(/^./, (match) => match.toLowerCase());
   }
 }
